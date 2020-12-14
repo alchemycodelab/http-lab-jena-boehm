3adcb17a824e8c64327ef79fd7c9748258876cb1
Jena Boehm <boehmjena@gmail.com>
2020-12-14 15:27:58 -0800

    get route passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('returns hi via GET', async() => {
+    const response = await request(app)
+      .get('/');
 
+    expect(response.text).toEqual('hi');
+  });
 });
