e1de41f3aa8125da9acd0026dc2280664570682c
Jena Boehm <boehmjena@gmail.com>
2020-12-14 16:12:52 -0800

    /echo test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,4 +8,11 @@ describe('app routes', () => {
 
     expect(response.text).toEqual('hi');
   });
+
+  it('returns status code 200 and plain text with the request body via POST', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send('hi');
+    expect(response.text).toEqual('hi');
+  });
 });
